c94e7f0fc3d7c2c5d70943a03b312b3275f28fca
ylzha <118960192+ylzhaqiang@users.noreply.github.com>
2026-04-26 12:26:25 +0800

    Update grid-view.tsx

--- src/app/share/grid-view.tsx
+++ src/app/share/grid-view.tsx
@@ -56,7 +56,7 @@ export default function GridView({ shares, isEditMode = false, onUpdate, onDelet
 				</div>
 			</div>
 
-			<div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+			<div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5'>
 				{filteredShares.map(share => (
 					<ShareCard key={share.url} share={share} isEditMode={isEditMode} onUpdate={onUpdate} onDelete={() => onDelete?.(share)} />
 				))}
